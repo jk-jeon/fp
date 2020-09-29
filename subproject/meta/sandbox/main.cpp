cf29143372ad06cb18db6f75b375f29a362281f1
Junekey Jeon <jk_jeon@kaist.ac.kr>
2020-09-29 15:19:15 -0700

    Quick benchmark against ryu's d2exp_buffered_n

--- subproject/meta/sandbox/main.cpp
+++ subproject/meta/sandbox/main.cpp
@@ -11,10 +11,14 @@ namespace jkj::fp {
 	}
 }
 
+#include <chrono>
 #include <iostream>
 #include <iomanip>
+#include <string>
 #include <string_view>
-
+#include <stdexcept>
+#include <memory>
+#include <vector>
 
 #include "ryu/ryu.h"
 
@@ -52,5 +56,67 @@ int main()
 	}
 #else
 	// Benchmark
+	constexpr std::size_t number_of_samples = 100000;
+	auto running_time = std::chrono::seconds(5);
+	auto buffer = std::make_unique<char[]>(10000);
+
+	// Generate random inputs
+	std::cout << "Generating samples...\n";
+	auto rg = jkj::fp::detail::generate_correctly_seeded_mt19937_64();
+	std::vector<double> samples(number_of_samples);
+	for (std::size_t i = 0; i < number_of_samples; ++i) {
+		samples[i] = jkj::fp::detail::uniformly_randomly_generate_general_float<double>(rg);
+	}
+	std::cout << "Done.\n\n\n";
+
+	while (true) {
+		int precision;
+		std::string str;
+		while (true) {
+			std::cout << "Input precision: ";
+			std::getline(std::cin, str);
+			try {
+				precision = std::stoi(str);
+				break;
+			}
+			catch (std::out_of_range const&) {
+				std::cout << "Invalid input.\n";
+			}
+		}
+
+		// Run benchmark
+		std::size_t sample_idx = 0;
+		std::size_t iteration_count = 0;
+		auto from = std::chrono::steady_clock::now();
+		auto to = from + running_time;
+		std::chrono::time_point now = std::chrono::steady_clock::now();
+		while (now <= to) {
+			d2exp_buffered_n(samples[sample_idx], precision, buffer.get());
+			if (++sample_idx == number_of_samples) {
+				sample_idx = 0;
+			}
+			++iteration_count;
+			now = std::chrono::steady_clock::now();
+		}
+		auto elapsed = double(std::chrono::duration_cast<std::chrono::nanoseconds>(now - from).count());
+		std::cout << "Ryu: " << (elapsed / double(iteration_count)) << "ns per iteration\n";
+
+		sample_idx = 0;
+		iteration_count = 0;
+		from = std::chrono::steady_clock::now();
+		to = from + running_time;
+		now = std::chrono::steady_clock::now();
+		while (now <= to) {
+			jkj::fp::to_chars_fixed_precision_scientific(samples[sample_idx], buffer.get(), precision);
+			if (++sample_idx == number_of_samples) {
+				sample_idx = 0;
+			}
+			++iteration_count;
+			now = std::chrono::steady_clock::now();
+		}
+		elapsed = double(std::chrono::duration_cast<std::chrono::nanoseconds>(now - from).count());
+		std::cout << " fp: " << (elapsed / double(iteration_count)) << "ns per iteration\n";
+	}
+
 #endif
 }
